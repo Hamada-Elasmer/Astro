5a10295059552558227830df6211add4de99f812
Hamada Elasmer <55856924+H-Elasmer@users.noreply.github.com>
2021-01-12 01:49:58 +0200

    Setup & Folder Structure

---
+++ gulpfile.js
@@ -0,0 +1,71 @@
+const gulp = require("gulp");
+const pug = require('gulp-pug');
+const concat = require('gulp-concat');
+const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const sourcemaps = require("gulp-sourcemaps");
+const { src, series, parallel, dest, watch } = require("gulp");
+
+
+// Path To Our Files
+const pugPath = 'src/pug/*.pug';
+const sassPath = 'src/scss/**/*.scss';
+const jsPath = 'src/js/**/*.js';
+
+
+// Pug Function
+function pugTask () {
+    return src(pugPath)
+        .pipe(pug({pretty: true}))
+        .pipe(dest('./build'));
+}
+// Sass Function
+function sassTask () {
+    return src(sassPath)
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'expand'}).on('error', sass.logError))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('./build/css'))
+}
+// JavaScript Function
+function jsTask () {
+    return src(jsPath)
+        .pipe(sourcemaps.init())
+        .pipe(concat('astro.bundle.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('./build/js'));
+}
+// Bootsrtap Js Move To build File Function
+function jsbootstarp () {
+    return src([
+        "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js",
+        "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map"
+      ])
+        .pipe(dest('./build/js'))
+}
+// imageMin Function
+function imageTask () {
+    return src('src/images/**/*')
+        .pipe(imagemin())
+        .pipe(dest('./build/images'));
+}
+// Move Font-Awesome Function
+function fontTask () {
+    return src([
+        './node_modules/@fortawesome/fontawesome-free/webfonts/*'
+    ])
+    .pipe(dest('./build/fonts'))
+}
+
+//  Watch Function
+function watchTask () {
+    watch([pugPath, sassPath, jsPath] , parallel(pugTask, sassTask, jsTask))
+}
+
+exports.pugTask = pugTask;
+exports.sassTask = sassTask;
+exports.imageTask = imageTask;
+exports.jsTask = jsTask;
+exports.jsbootstarp = jsbootstarp;
+exports.fontTask = fontTask;
+exports.default = series(parallel(pugTask, sassTask, jsTask), watchTask);
---
+++ src/js/astro.js
@@ -0,0 +1,22 @@
+$(function (){
+    'use strict';
+
+    // :: 0.1 Preloader Active Code
+    $(window).on('load', function () {
+        $('#preloader').fadeOut('1000', function () {
+            $(this).remove();
+        });
+    });
+    // :: 0.2 Scrollup Active Code
+    if ($.fn.scrollUp) {
+        $(window).scrollUp({
+            // scrollSpeed: 500,
+            scrollText: '<i class="fa fa-angle-up"</i>'
+        });
+    }
+    // :: 0.3 Prevent Default 'a' Click
+    $('a[href="#"]').on('click', function ($) {
+        $.preventDefault();
+    });
+
+});
